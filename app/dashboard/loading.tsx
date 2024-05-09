b68f407ab7b6f300e64ec5d12c5b83d69932673b
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-09 22:01:53 +0200

    Adding loading skeletons

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,5 +1,7 @@
 // app/dashboard/loading.tsx
 
+import DashboardSkeleton from '@/app/ui/skeletons';
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <DashboardSkeleton />;
 }
